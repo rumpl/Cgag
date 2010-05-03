75fe7b933c1847db31cb0882526743ef506f1bea
unknown <dlukic@.crm.local>
2010-05-03 14:45:36 +0200

    Added the PokemonExceptionHandling.

---
+++ Cgag/Attributes/Disclaimer/PokemonExceptionHandling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cgag.Attributes.Disclaimer
+{
+  [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Delegate | AttributeTargets.Property)]
+  public class PokemonExceptionHandling : Attribute
+  {
+    public string Value { get; set; }
+
+    public PokemonExceptionHandling()
+    { }
+
+    public PokemonExceptionHandling(string value)
+    {
+      Value = value;
+    }
+  }
+}
